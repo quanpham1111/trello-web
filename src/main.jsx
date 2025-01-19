9ff0702e7ba7086bc1121cb5ed9c74e28d8a61c9
Nguyen Quan <nguyenquan10k4@gmail.com>
2025-01-19 20:11:26 +0700

    Config MUI Theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CssBaseline />
-    {/* The rest of my application */}
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {/* The rest of my application 1*/}
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
