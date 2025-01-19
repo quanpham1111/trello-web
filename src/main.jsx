33d9322ca1ceacce8c6d1a176a5bbfb0ae62efcd
Nguyen Quan <nguyenquan10k4@gmail.com>
2025-01-19 18:33:41 +0700

    Add_CssBaseline

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import CssBaseline from '@mui/material/CssBaseline'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <CssBaseline />
+    {/* The rest of my application */}
     <App />
   </React.StrictMode>
 )
