d3076bf362205ce90b1b448deb5e91a31bdc3ae9
Nguyen Quan <nguyenquan10k4@gmail.com>
2025-01-24 20:45:46 +0700

    Import_AbsoluteConfig

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,5 +1,5 @@
 import Box from '@mui/material/Box'
-import ModeSelect from '../../components/ModeSelect'
+import ModeSelect from '/src/components/ModeSelect'
 function AppBar() {
   return (
     <Box sx={{
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from '~/App.jsx'
 import CssBaseline from '@mui/material/CssBaseline'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import theme from './theme'
