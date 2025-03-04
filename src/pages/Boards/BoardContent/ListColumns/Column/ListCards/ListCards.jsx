cefdf63c46b5c60ad4c23e9e3008959ab6d63ab5
Nguyen Quan <nguyenquan10k4@gmail.com>
2025-03-04 11:42:32 +0700

    drag drop card p1

--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
@@ -1,39 +1,42 @@
 
 import Card from './Card/Card'
 import Box from '@mui/material/Box'
-
+import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable'
 function ListCards({ cards }) {
   return (
-    <Box sx={{
-      //padding và margin cho thanh scroll bar và nội dung
-      p: '0 5px',
-      m: '0 5px',
-      display:'flex',
-      flexDirection:'column',
-      gap: 1,
-      //ẩn thánh scroll ngang
-      overflowX: 'hidden',
-      //tự auto scroll khi list card vượt max height
-      overflowY:'auto',
-      //tính chiều cao cho phần list card
-      maxHeight: (theme) => `calc(
-      ${theme.trello.boardContentHeight} - 
-      ${theme.spacing(5)} - 
-      ${theme.trello.columnHeaderHeight} -
-      ${theme.trello.columnFooterHeight}
-      )`,
-      //Css cho thanh scroll bar
-      '&::-webkit-scrollbar-thumb': {
-        backgroundColor: '#ced0da',
-        borderRadius:'8px'
-      },
-      '&::-webkit-scrollbar-thumb:hover': {
-        backgroundColor : 'white'
-      }
-    }}>
-      {/* Card 1 */}
-      {cards?.map( card => <Card key={card._id} card ={card}/> )}
-    </Box> )
+    <SortableContext items={cards?.map(c => c._id)} strategy={verticalListSortingStrategy}>
+      <Box sx={{
+        //padding và margin cho thanh scroll bar và nội dung
+        p: '0 5px',
+        m: '0 5px',
+        display:'flex',
+        flexDirection:'column',
+        gap: 1,
+        //ẩn thánh scroll ngang
+        overflowX: 'hidden',
+        //tự auto scroll khi list card vượt max height
+        overflowY:'auto',
+        //tính chiều cao cho phần list card
+        maxHeight: (theme) => `calc(
+        ${theme.trello.boardContentHeight} - 
+        ${theme.spacing(5)} - 
+        ${theme.trello.columnHeaderHeight} -
+        ${theme.trello.columnFooterHeight}
+        )`,
+        //Css cho thanh scroll bar
+        '&::-webkit-scrollbar-thumb': {
+          backgroundColor: '#ced0da',
+          borderRadius:'8px'
+        },
+        '&::-webkit-scrollbar-thumb:hover': {
+          backgroundColor : 'white'
+        }
+      }}>
+        {/* Card 1 */}
+        {cards?.map( card => <Card key={card._id} card ={card}/> )}
+      </Box>
+    </SortableContext>
+  )
 }
 
 export default ListCards
